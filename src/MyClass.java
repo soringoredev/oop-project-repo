94fa69ecc8ffe95b8db2a47ae5f6213e76ff1895
Sorin Gore <soringoredev@gmail.com>
2025-10-04 13:16:47 +0300

    Add file MyClass.java

---
+++ src/MyClass.java
@@ -0,0 +1,11 @@
+public class MyClass {
+    public static void main(String[] args) {
+
+        Point p = new Point();
+        p.x = 10;
+        p.y = 20;
+
+        System.out.println(p.x + p.y); // 10 20
+
+    }
+}
