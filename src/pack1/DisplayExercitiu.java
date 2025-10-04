72b15c51bfa416b86e71c04f31d7e05948e25f3a
Sorin Gore <soringoredev@gmail.com>
2025-10-04 13:35:08 +0300

    Add first step of exercise

---
+++ src/pack1/DisplayExercitiu.java
@@ -0,0 +1,12 @@
+package pack1;
+
+public class DisplayExercitiu {
+    public static void main(String[] args) {
+
+        Exercitiu afisare = new Exercitiu();
+
+        System.out.println(afisare.nr1);
+        System.out.println(afisare.nr2);
+
+    }
+}
---
+++ src/pack1/Exercitiu.java
@@ -0,0 +1,9 @@
+package pack1;
+
+public class Exercitiu {
+
+    public int nr1 = 100;
+    public int nr2 = 200;
+
+
+}
