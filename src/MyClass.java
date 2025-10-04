1144881be0db7cbd8e813fca243891eb6227a8d1
Sorin Gore <soringoredev@gmail.com>
2025-10-04 13:18:41 +0300

    Add new object

--- src/MyClass.java
+++ src/MyClass.java
@@ -2,10 +2,16 @@ public class MyClass {
     public static void main(String[] args) {
 
         Point p = new Point();
+        Point p2 = new Point();
+
         p.x = 10;
         p.y = 20;
 
-        System.out.println(p.x + p.y); // 10 20
+        p2.x = 30;
+        p2.y = 40;
+
+        System.out.println(p.x + " " + p.y); // 10 20
+        System.out.println(p2.x + " " + p2.y); // 30 40
 
     }
 }
